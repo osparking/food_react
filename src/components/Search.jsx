aa12c84e778fe4bafa8e9ab35a9e5839e664f819
osparking <jbpark03@gmail.com>
2024-11-02 14:43:51 +0900

    simplest useEffect react hook

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Search() {
   const [query, setQuery] = useState("pasta");
+  useEffect(() => {
+    function demo() {
+      console.log("demo called");
+    }
+    demo();
+  }, []);
   return (
     <div>
       <input
