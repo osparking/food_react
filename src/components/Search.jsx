bb6fae736ee05c9cc8e12620ad4e8de79eb6362a
osparking <jbpark03@gmail.com>
2024-11-02 17:55:47 +0900

    constants for URL and fetchRecipe() skeleton

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "142f1a88ecd044fbb5f387d790108686";
 
 export default function Search() {
   const [query, setQuery] = useState("pasta");
-  useEffect(() => {
-    function demo() {
-      console.log("demo called");
-    }
-    demo();
-  }, [query]);
+  useEffect(
+    (e) => {
+      function fetchRecipe() {
+      }
+    },
+    [query]
+  );
   return (
     <div>
       <input
