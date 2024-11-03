587d97375228bb6d68515c54c963e95dc5533a5c
osparking <jbpark03@gmail.com>
2024-11-03 19:51:33 +0900

    more hamburger emojis

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import styles from "./nav.module.css";
 
 export default function Nav() {
-  return <div className={styles.nav}>🍔조리법/레시피 앱</div>;
+  return <div className={styles.nav}>🍔🍔조리법/레시피 앱🍔🍔</div>;
 }
