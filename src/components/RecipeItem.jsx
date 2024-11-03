acceb74171b0aa9c206f17fbdce65f825aa48068
osparking <jbpark03@gmail.com>
2024-11-03 21:37:06 +0900

    make image not exceed div(container)

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -3,7 +3,7 @@ import styles from './recipeitem.module.css';
 export default function RecipeItem({ recipe }) {
   return (
     <div className={styles.recipeContainer}>
-      <img src={pizzaPumpkin} alt="피자와 단호박" />
+      <img className={styles.foodImage} src={pizzaPumpkin} alt="피자와 단호박" />
       <h1>피자와 단호박</h1>
       <button onClick={null}>조리법</button>
       <br />
