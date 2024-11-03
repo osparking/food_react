a9f11b022a89dfd3d391d8c1ff174334d6d3cd36
osparking <jbpark03@gmail.com>
2024-11-03 21:09:46 +0900

    set recipe container width to 300px

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -1,6 +1,7 @@
+import styles from "./recipeitem.module.css";
 export default function RecipeItem({ recipe }) {
   return (
-    <div style={{marginTop: '20px'}}>
+    <div className={styles.recipeContainer}>
       <img src={recipe.image} alt={recipe.title} />
       <h1>{recipe.title}</h1>
       <button onClick={null}>조리법</button>
