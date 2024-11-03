bd2419af229f48bd1cb2fb61033d770af56c0480
osparking <jbpark03@gmail.com>
2024-11-04 08:42:05 +0900

    create div for text context, put name in

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -3,7 +3,9 @@ export default function RecipeItem({ recipe }) {
   return (
     <div className={styles.recipeContainer}>
       <img className={styles.foodImage} src={recipe.image} alt={recipe.title} />
-      <h1>{recipe.title}</h1>
+      <div className={styles.recipeText}>
+        <p className={styles.recipeName}>피자와 단호박</p>
+      </div>
       <button onClick={null}>조리법</button>
       <br />
     </div>
