df65bc0b574b97f967dd7df191132c21b51d3cea
osparking <jbpark03@gmail.com>
2024-11-03 12:58:34 +0900

    App uses RecipeList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import Search from "./components/Search";
+import RecipeList from "./components/RecipeList";
 
 function App() {
   const [recipeData, setRecipeData] = useState([]);
   return (
     <div className="App">
       <Search recipeData={recipeData} setRecipeData={setRecipeData} />
-      {recipeData.map((reccipe) => (
-        <h1>{reccipe.title}</h1>
-      ))}
+      <RecipeList recipeData={recipeData} />
     </div>
   );
 }
