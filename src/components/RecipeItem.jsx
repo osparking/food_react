1cc66b9bf3e58a23ebd31629d8325efe43452adf
osparking <jbpark03@gmail.com>
2024-11-04 08:42:05 +0900

    create div for text context, put name in

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -1,10 +1,16 @@
-import pizzaPumpkin from './images/pizza-pumpkin.jpg'
-import styles from './recipeitem.module.css';
+import pizzaPumpkin from "./images/pizza-pumpkin.jpg";
+import styles from "./recipeitem.module.css";
 export default function RecipeItem({ recipe }) {
   return (
     <div className={styles.recipeContainer}>
-      <img className={styles.foodImage} src={pizzaPumpkin} alt="피자와 단호박" />
-      <h1>피자와 단호박</h1>
+      <img
+        className={styles.foodImage}
+        src={pizzaPumpkin}
+        alt="피자와 단호박"
+      />
+      <div className={styles.recipeText}>
+        <p className={styles.recipeName}>피자와 단호박</p>
+      </div>
       <button onClick={null}>조리법</button>
       <br />
     </div>
