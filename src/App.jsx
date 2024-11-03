49d57fe22b53d9c5f6422befc936a12fd6b81144
osparking <jbpark03@gmail.com>
2024-11-03 12:17:25 +0900

    show each recipe title in <h1> tag

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <Search recipeData={recipeData} setRecipeData={setRecipeData} />
+      {recipeData.map((reccipe) => (
+        <h1>{reccipe.title}</h1>
+      ))}
     </div>
   );
 }
