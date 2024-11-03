bccf98c3fbf4d2650bf505142cc3ce7893584cd6
osparking <jbpark03@gmail.com>
2024-11-03 21:09:46 +0900

    set recipe container width to 300px

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -1,7 +1,8 @@
 import pizzaPumpkin from './images/pizza-pumpkin.jpg'
+import styles from './recipeitem.module.css';
 export default function RecipeItem({ recipe }) {
   return (
-    <div style={{marginTop: '20px'}}>
+    <div className={styles.recipeContainer}>
       <img src={pizzaPumpkin} alt="피자와 단호박" />
       <h1>피자와 단호박</h1>
       <button onClick={null}>조리법</button>
