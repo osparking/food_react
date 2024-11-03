bfe3f4d69112347b734156ccd9b5fb192c1a596d
osparking <jbpark03@gmail.com>
2024-11-03 11:59:10 +0900

    declare recipe data state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Search from "./components/Search";
 
 function App() {
+  const [recipeData, setRecipeData] = useState([]);
   return (
     <div className="App">
       <Search />
