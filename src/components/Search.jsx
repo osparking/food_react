678e9e07be8c3a75d5a58c2079a1971de441e1c0
osparking <jbpark03@gmail.com>
2024-11-03 10:58:27 +0900

    using environment variable in vite react

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
-const API_KEY = "142f1a88ecd044fbb5f387d790108686";
 
 export default function Search() {
   const [query, setQuery] = useState("pasta");
   useEffect(
     (e) => {
       async function fetchRecipe() {
-        const response = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+        const response = await fetch(
+          `${URL}?query=${query}&apiKey=${import.meta.env.VITE_API_KEY}`
+        );
         const recipies = await response.json();
         console.log(recipies.results);
       }
