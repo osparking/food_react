0f7650697a0e1041a02c19f1c5ce2843e5ff91e3
osparking <jbpark03@gmail.com>
2024-11-04 21:14:00 +0900

    fetch recipe details of clickedId

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,3 +1,18 @@
+import { useEffect } from "react";
 export default function RecipeDetails({ clickedId }) {
+  const URL = `https://api.spoonacular.com/recipes/${clickedId}/information`;
+  useEffect(
+    (e) => {
+      async function fetchDetails() {
+        const response = await fetch(
+          `${URL}?apiKey=${import.meta.env.VITE_API_KEY}`
+        );
+        const recipeDetails = await response.json();
+        console.log(recipeDetails);
+      }
+      fetchDetails();
+    },
+    []
+  );
   return <div>상세 조리법: {clickedId}</div>;
 }
