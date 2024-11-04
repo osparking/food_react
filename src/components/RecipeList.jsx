cafd81f96e9ced7e6e6226366623432b21c64171
osparking <jbpark03@gmail.com>
2024-11-04 19:55:34 +0900

    list transfers state variable to item

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,10 +1,14 @@
 import RecipeItem from "./RecipeItem";
 
-export default function RecipeList({ recipeData }) {
+export default function RecipeList({ recipeData, setClickedId }) {
   return (
     <div>
       {recipeData.map((recipe) => (
-        <RecipeItem recipe={recipe} key={recipe.id} />
+        <RecipeItem
+          setClickedId={setClickedId}
+          recipe={recipe}
+          key={recipe.id}
+        />
       ))}
     </div>
   );
