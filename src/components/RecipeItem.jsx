4312f6bfb64d4510603d4e48a1dea4be9e6d363b
osparking <jbpark03@gmail.com>
2024-11-04 10:41:23 +0900

    constant recipe name/title made dynamic

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -4,7 +4,7 @@ export default function RecipeItem({ recipe }) {
     <div className={styles.recipeContainer}>
       <img className={styles.foodImage} src={recipe.image} alt={recipe.title} />
       <div className={styles.recipeText}>
-        <p className={styles.recipeName}>피자와 단호박</p>
+        <p className={styles.recipeName}>{recipe.title}</p>
       </div>
       <button onClick={null}>조리법</button>
       <br />
