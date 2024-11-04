ad6a2d684ce74a7111baa2463d2bdd0077a1e14c
osparking <jbpark03@gmail.com>
2024-11-04 19:51:16 +0900

    define and send clicked recipe ID

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import "./App.css";
 import Container from "./components/Container";
 import InnerContainer from "./components/InnerContainer";
 import Nav from "./components/Nav";
+import RecipeDetails from "./components/RecipeDetails";
 import RecipeList from "./components/RecipeList";
 import Search from "./components/Search";
-import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   const [recipeData, setRecipeData] = useState([]);
+  const [clickedId, setClickedId] = useState("");
   return (
     <div className="App">
       <Nav />
       <Search recipeData={recipeData} setRecipeData={setRecipeData} />
       <Container>
         <InnerContainer>
-          <RecipeList recipeData={recipeData} />
+          <RecipeList setClickedId={setClickedId} recipeData={recipeData} />
         </InnerContainer>
         <InnerContainer>
-          <RecipeDetails/>
+          <RecipeDetails clickedId={clickedId} />
         </InnerContainer>
       </Container>
     </div>
