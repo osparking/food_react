4e6449d721af427a54dcdc345046561288d2de67
osparking <jbpark03@gmail.com>
2024-11-05 14:12:33 +0900

    read recipe details file locally

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,19 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import details from "../local/details.json";
 export default function RecipeDetails({ clickedId }) {
   const URL = `https://api.spoonacular.com/recipes/${clickedId}/information`;
+  const [reDetails, setReDetails] = useState({});
   useEffect(
     (e) => {
       async function fetchDetails() {
         if (clickedId === "") return;
-        const response = await fetch(
-          `${URL}?apiKey=${import.meta.env.VITE_API_KEY}`
-        );
-        const recipeDetails = await response.json();
-        console.log(recipeDetails);
+        // const response = await fetch(
+        //   `${URL}?apiKey=${import.meta.env.VITE_API_KEY}`
+        // );
+        // const recipeDetails = await response.json();
+        details && (setReDetails(details), console.log("디테일: ", details));
       }
       fetchDetails();
     },
     [clickedId]
   );
-  return <div>상세 조리법: {clickedId}</div>;
+  return (
+    <div>
+      {reDetails.title && (
+        <div>
+          {reDetails.title}
+          <img src={reDetails.image} alt={reDetails.title} />
+        </div>
+      )}
+      <div>상세 조리법: {clickedId}</div>
+    </div>
+  );
 }
