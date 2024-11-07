7da16e6a66d488dc29290ffb5ef402c32a9d749a
osparking <jbpark03@gmail.com>
2024-11-07 12:25:02 +0900

    ingredient div restructured, classNames added

--- src/components/Ingredient.jsx
+++ src/components/Ingredient.jsx
@@ -1,16 +1,24 @@
+import styles from './ingredient.module.css'
 export default function Ingredient({ ingredient }) {
   return (
     <div>
-      <img
-        src={
-          `https://img.spoonacular.com/ingredients_100x100/` + ingredient.image
-        }
-        alt={ingredient.image}
-      />
-      <h3>{ingredient.name}</h3>
-      <h3>
-        {ingredient.amount} {ingredient.unit}{" "}
-      </h3>
+      <div className={styles.containAll}>
+        <div className={styles.containImage}>
+          <img
+            src={
+              `https://img.spoonacular.com/ingredients_100x100/` +
+              ingredient.image
+            }
+            alt={ingredient.image}
+          />
+        </div>
+        <div className={styles.containEtc}>
+          <div className={styles.name}>{ingredient.name}</div>
+          <div className={styles.amount}>
+            {ingredient.amount} {ingredient.unit}{" "}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
